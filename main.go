c46e777fc9884fd3ff76aa921f7bb912b8899606
Nicolás Serna <nicolito128@hotmail.com>
2022-03-27 20:38:03 -0300

    Use tasks package in main

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/nicolito128/gin-apitest/tasks"
 )
 
 func main() {
@@ -21,13 +22,13 @@ func main() {
 	router.Static("/static/styles", "./static/styles")
 
 	router.GET("/", indexEndpoint)
+	router.GET("/tasks", tasks.GetTasksEndpoint)
+	router.POST("/tasks", tasks.PostTaskEndpoint)
 
 	// Start server
 	router.Run()
 }
 
 func indexEndpoint(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.html", gin.H{
-		"title": "Welcome! - Go Test",
-	})
+	c.HTML(http.StatusOK, "index.html", gin.H{})
 }
