17cf1eddaa342913c5d12c102525ce731548d123
Nicolás Serna <nicolito128@hotmail.com>
2022-04-02 02:36:12 -0300

    Update main

--- main.go
+++ main.go
@@ -1,21 +1,15 @@
 package main
 
 import (
-	"log"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
-	"github.com/nicolito128/gin-apitest/pkg/database"
 	"github.com/nicolito128/gin-apitest/pkg/tasks"
 )
 
 func main() {
 	router := gin.New()
-	_, err := database.Init()
-	if err != nil {
-		log.Fatalf("%s", err)
-	}
 
 	// Middleware
 	router.Use(gin.Logger())
@@ -35,11 +29,11 @@ func main() {
 	router.LoadHTMLGlob("./templates/*")
 
 	router.GET("/", indexEndpoint)
-	router.GET("/tasks", tasks.GetTasks)
-	router.GET("/tasks/:id", tasks.FindTask)
-	router.POST("/tasks", tasks.CreateTask)
-	router.DELETE("/tasks/:id", tasks.DeleteTask)
-	router.PUT("/tasks/:id", tasks.UpdateTask)
+	router.GET("/tasks", tasks.GetAllEndpoint)
+	router.GET("/tasks/:id", tasks.FindEndpoint)
+	router.POST("/tasks", tasks.CreateEndpoint)
+	router.DELETE("/tasks/:id", tasks.DeleteEndpoint)
+	router.PUT("/tasks/:id", tasks.UpdateEndpoint)
 
 	router.Run()
 }
